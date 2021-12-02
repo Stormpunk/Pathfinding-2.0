25ca316a2ea95ab021735e09ff6c29e692d65dc1
Stormpunk <andjkim1@gmail.com>
2021-12-02 16:32:43 +1100

    Initial Commit
    Setup, no AI work yet

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform endGoal;
+    public Transform key;
+    public Transform gold;
+    public bool hasCollectedGold;
+    public bool hasKey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
